4eb6f6de746fa597e3aa3906a80f7655dd3d5389
valeriia-danylenko <80961981+valeriia-danylenko@users.noreply.github.com>
2021-05-11 21:55:29 +0300

    Update 20210508183142-orders-create.js

--- Demo2.1/db/migrations/20210508183142-orders-create.js
+++ Demo2.1/db/migrations/20210508183142-orders-create.js
@@ -4,7 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.createTable('orders', {
       id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-      user_id: { type: Sequelize.INTEGER, allowNull: false, references: { model: 'User', key: 'id' }, onUpdate: 'CASCADE', onDelete: 'SET NULL' },
+      user_id: { type: Sequelize.INTEGER, allowNull: false, references: { model: 'users', key: 'id' }, onUpdate: 'CASCADE', onDelete: 'SET NULL' },
       time: { type: Sequelize.DATE, defaultValue: Sequelize.NOW }
     })
   },
