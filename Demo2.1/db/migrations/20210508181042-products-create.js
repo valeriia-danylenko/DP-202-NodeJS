140510bda3a37e6ca63dac94f9af4739da7749cf
valeriia-danylenko <80961981+valeriia-danylenko@users.noreply.github.com>
2021-05-12 14:36:30 +0300

    Update 20210508181042-products-create.js

--- Demo2.1/db/migrations/20210508181042-products-create.js
+++ Demo2.1/db/migrations/20210508181042-products-create.js
@@ -10,7 +10,7 @@ module.exports = {
       ingridients: { type: Sequelize.STRING(1000) },
       amount: { type: Sequelize.INTEGER, allowNull: false },
       units_id: { type: Sequelize.INTEGER, allowNull: false, references: { model: 'units', key: 'id' }, onUpdate: 'CASCADE', onDelete: 'SET NULL' },
-      price: { type: DataTypes.REAL, allowNull: false },
+      price: { type: Sequelize.REAL, allowNull: false },
       img_link: { type: Sequelize.STRING(500), allowNull: false }
     });
   },
